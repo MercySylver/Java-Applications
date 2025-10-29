10f4d685c055e8a647bfbbc6e5ff9581b2c7edef
MercySylver <mercyben405@gmail.com>
2025-10-29 12:06:30 +0100

    Selection part1

--- OperatorsPart1.java
+++ OperatorsPart1.java
@@ -27,13 +27,32 @@ public class OperatorsPart1{
 		int product = num1 * num2;
 		int minus = num1 - num2;
 		double divide = (double)num1/num2;
+		int modulos = num1%num2;
 		
 		System.out.printf("Sum is %d%n",sum);
 		System.out.printf("product is %d%n",product);
 		System.out.printf("difference btw num1 and num2 is %d%n",minus);
 		System.out.println(name + sum);
 		System.out.printf("Result is %f",divide);
+		System.out.printf("Modulos is %d",modulos);
 		
+		//Relational Operators
+		int firstNum =12;
+		int secondNum = 15;
+		boolean isGreater = firstNum > secondNum;
+		boolean isLesser = firstNum < secondNum;
+		boolean isGreaterThanOrEqualTo = firstNum >= secondNum;
+		boolean isLessThanOrEqualTo = firstNum <= secondNum;
+		boolean isNotEqualTo = firstNum != secondNum;
+		boolean isEqualTo = firstNum == secondNum;
+		
+		
+		System.out.printf("is firstNum > secondNum? %b%n",isGreater);
+		System.out.printf("is firstNum < secondNum? %b%n",isLesser);
+		System.out.printf("is firstNum >= secondNum? %b%n",isGreaterThanOrEqualTo);
+		System.out.printf("is firstNum < secondNum? %b%n",isLessThanOrequalTo);
+		System.out.printf("is firstNum != secondNum? %b%n",isNotEqualTo);
+		System.out.printf("is firstNum == secondNum? %b%n",isEqualTo);
 		
 		
 		
